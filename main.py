57e38b5e28d5d5d15378dbf54a52675a6feb01a9
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2022-06-06 21:22:53 -0700

    Updated to provide NULL data for adding + cleaned error mistakes.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ while True:
         """
         A: Add task
         B: Print tasks
+        C: Update tasks
         """
     )
     option = input('Select one of the above options: ')
@@ -22,46 +23,20 @@ while True:
     if option == 'a':
         new_task = input('What task would you like to add? ')
         task_description = input('Provide a brief description to the task: ')
-        start_month = validation.month_response('start')
-        start_date = validation.day_response('start')
-        start_year = validation.year_response('start')
-        due_month = validation.month_response('finish')
-        due_date = validation.day_response('finish')
-        due_year = validation.year_response('finish')
+        # start_month = validation.month_response('start')
+        # start_date = validation.day_response('start')
+        # start_year = validation.year_response('start')
+        # due_month = validation.month_response('finish')
+        # due_date = validation.day_response('finish')
+        # due_year = validation.year_response('finish')
         task_collection.add_task(new_task,
                                  task_description,
-                                 datetime(start_year, start_month, start_date),
-                                 datetime(due_year, due_month, due_date))
+                                 # datetime(start_year, start_month, start_date),
+                                 # datetime(due_year, due_month, due_date)
+        )
 
     elif option == 'b':
         task.TaskCollection.print_task(task_collection)
 
+    elif option == 'c':
 
-# task_collection = task.TaskCollection()
-#
-# try:
-#     max_id = select(max([task_collection.database.c.task_id]) + 1)
-# except:
-#     max_id = 1
-#
-# task_collection.add_task(1, 'complete homework', 'for python class',datetime(2023,1,1), datetime(2023,6,1))
-
-# ins = task_collection.database.insert().values(task_id=1,
-#                            task='Complete Homework',
-#                            task_description='for Python class',
-#                            start_date=datetime(2023,1,1),
-#                            due_date=datetime(2023,6,1))
-
-
-# # session.query(func.max(task_collection.task_id))
-# # new_task = input('What task do you want to add? ')
-# # task_description = input('Provide a brief description to the task ')
-# # start_date = input('When do you want to start on this task? ')
-# # due_date = input('When do you want to finish this task? ')
-# #
-# # task_collection.add_task(new_task, task_description, start_date, due_date)
-# s = task_collection.select()
-# result = model.conn.execute(s)
-# row = result.fetchone()
-# for x in row:
-#    print(x)
--- task.py
+++ task.py
@@ -12,28 +12,35 @@ class TaskCollection:
     def __init__(self):
         self.database = model.task_detail
 
-    def add_task(self, new_task, task_description, start_date, due_date):
+    def add_task(self, new_task, task_description):#, start_date, due_date):
         select_max = conn.execute(select(max([self.database.c.task_id])))
-        new_id = select_max.fetchone()
+        id_results = select_max.fetchone()
         try:
-            new_id += 1
-        except TypeError:
+            new_id = id_results.task_id + 1
+        except AttributeError:
             new_id = 1
+        # try:
+        #     new_id = id_results.task_id + 1
+        # except TypeError:
+        #     new_id = 1
         ins = insert(self.database).values(task_id=new_id,
-                                     task=new_task,
-                                     task_description=task_description,
-                                     start_date=start_date,
-                                     due_date=due_date)
+                                           task=new_task,
+                                           task_description=task_description,
+                                           start_date=None,
+                                           due_date=None
+                                             # start_date=start_date,
+                                             # due_date=due_date
+                                                   )
         conn.execute(ins)
         conn.commit()
 
     def print_task(self):
-        try:
-            s = select(self.database.c.task)
-            select_result = conn.execute(s)
-            row = select_result.fetchone()
-            print('The following tasks have been added')
+        s = select(self.database.c.task_id, self.database.c.task)
+        select_result = conn.execute(s)
+        row = select_result.fetchall()
+        print('The following tasks have been added:')
+        if len(row)>0:
             for x in row:
-                print(x)
-        except TypeError:
+                print(x.task_id, x.task)
+        else:
             print('None')
