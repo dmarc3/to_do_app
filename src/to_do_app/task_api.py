ee845cc188876b138769695d0d1a0f93e96dc886
kathleen101 <81065967+kathleen101@users.noreply.github.com>
2022-06-14 22:07:27 -0700

    Updated between call.

--- src/to_do_app/task_api.py
+++ src/to_do_app/task_api.py
@@ -24,7 +24,7 @@ class Priority(Resource):
 class DueDate(Resource):
     def get(self):
         task_collection = task.TaskCollection()
-        query = task_collection.sort_query('due_date', 'DESC')
+        query = task_collection.sort_query('due_date', 'ASC')
         result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query]}
         return jsonify(result)
 
@@ -32,16 +32,6 @@ class DueDate(Resource):
 class Between(Resource):
     def get(self):
         task_collection = task.TaskCollection()
-        dates = get_valid_input(
-            'start_date',
-            'REQUEST: What date would you like to start from (YYYY-MM-DD)?\nDATE: ',
-            )
-        # Get end
-        dates = get_valid_input(
-            'due_date',
-            'REQUEST: What date would you like to end at (YYYY-MM-DD)?\nDATE: ',
-            dates,
-        )
         query = task_collection.filter_closed_between_query(
             start=dates['start_date'], end=dates['due_date'])
         result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query]}
@@ -59,11 +49,21 @@ class Overdue(Resource):
 if __name__ == "__main__":
     db_connect = create_engine("sqlite:///task.db")
     app = Flask(__name__)
-    api = Api(app) # task 1
+    api = Api(app)
     api.add_resource(Task, '/tasks')  # task 1-sorted by task id
     api.add_resource(Priority, '/priority')  # task 2-sorted by priority
     api.add_resource(DueDate, '/due_date')  # task 3-sorted by due date
-    api.add_resource(Between, '/between')  # task 4-between two dates
+    dates = get_valid_input(
+        'start_date',
+        'REQUEST: What date would you like to start from (YYYY-MM-DD)?\nDATE: ',
+    )
+    # Get end
+    dates = get_valid_input(
+        'due_date',
+        'REQUEST: What date would you like to end at (YYYY-MM-DD)?\nDATE: ',
+        dates,
+    )
+    api.add_resource(Between, f'/between{dates["start_date"]}&{dates["due_date"]}')  # task 4-between two dates
     api.add_resource(Overdue, '/overdue')  # task 5-overdue
     app.run(port="5002")
     db_connect.dispose()
